cb2502fac5c54b56c5772ee210e76d0a23fb534f
Flexlug <34001155+Flexlug@users.noreply.github.com>
2021-02-19 21:03:32 +0300

    Revert "Tons of features are f*cked. I'm f*cked. Everything is f*cked. F*ck e…"

---
+++ WAV-Bot-DSharp/Migrations/20201006183725_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WAV_Bot_DSharp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<ulong>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Uid = table.Column<ulong>(nullable: false),
+                    LastActivity = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
