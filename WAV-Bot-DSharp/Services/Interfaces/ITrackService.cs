ad2f5078f3cc0fcdcc931083016eed0d59a930ea
Flexlug <flexlug@outlook.com>
2021-02-23 06:29:57 +0300

    Modyfied ITrackService for bancho.

--- WAV-Bot-DSharp/Services/Interfaces/ITrackService.cs
+++ WAV-Bot-DSharp/Services/Interfaces/ITrackService.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 
 using WAV_Osu_NetApi.Gatari.Models;
+using WAV_Osu_NetApi.Bancho.Models;
 
 namespace WAV_Bot_DSharp.Services.Interfaces
 {
@@ -22,5 +23,19 @@ namespace WAV_Bot_DSharp.Services.Interfaces
         /// <param name="u">Gatari user</param>
         /// <returns></returns>
         public Task RemoveTrackRecent(GUser u);
+
+        /// <summary>
+        /// Start user tracking
+        /// </summary>
+        /// <param name="u">Bancho user</param>
+        /// <returns></returns>
+        public Task AddTrackRecent(User u);
+
+        /// <summary>
+        /// Stop user tracking
+        /// </summary>
+        /// <param name="u">Bancho user</param>
+        /// <returns></returns>
+        public Task RemoveTrackRecent(User u);
     }
 }
