07ea752dece2a9db8ebe30592898d60190699199
Flexlug <flexlug@outlook.com>
2021-02-23 06:20:46 +0300

    Added TrackedUser

---
+++ WAV-Bot-DSharp/Services/Structures/TrackedUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WAV_Bot_DSharp.Services.Structures
+{
+    public class TrackedUser
+    {
+        /// <summary>
+        /// DataBase ID
+        /// </summary>
+        [Key]
+        public ulong Id { get; set; }
+
+        /// <summary>
+        /// Uid пользователя
+        /// </summary>
+        public ulong Uid { get; set; }
+
+        /// <summary>
+        /// Bancho id
+        /// </summary>
+        public int? banchoId { get; set; }
+
+        /// <summary>
+        /// Gatari id
+        /// </summary>
+        public int? gatariId { get; set; }
+    }
+}
