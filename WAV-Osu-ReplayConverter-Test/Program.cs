0f233633e7cb7c8fb5ee937fbb13c477d95614d6
Flexlug <flexlug@outlook.com>
2021-03-09 19:57:59 +0300

    Added WAV-Osu-ReplayConverter and WAV-Osu-ReplayConverter-Test

---
+++ WAV-Osu-ReplayConverter-Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WAV_Osu_ReplayConverter_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
