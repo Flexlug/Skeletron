57f35130e2f89786f3aa96797daa3446f098e9da
Flexlug <flexlug@outlook.com>
2021-04-23 21:52:08 +0300

    Smth went wrong. Mb later...

--- WAV-Bot-DSharp/Program.cs
+++ WAV-Bot-DSharp/Program.cs
@@ -16,8 +16,10 @@ namespace WAV_Bot_DSharp
             var settingsService = new SettingsLoader();
 
             Log.Logger = new LoggerConfiguration()
-                //.WriteTo.Console(new ExpressionTemplate ("{@t:HH:mm:ss} [{@l:u3}] [{Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1)}] {@m}\n{@x}"))
-                .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] [{SourceContext}] {Message}{NewLine}{Exception}")
+                //.WriteTo.Console(new ExpressionTemplate ("{@t:HH:mm:ss} [{@l:u3}] [{Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1)}] {@m}\n{@x}"),
+                //                 theme: AnsiConsoleTheme.Literate)
+                .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] [{SourceContext}] {Message}{NewLine}{Exception}",
+                                 theme: SystemConsoleTheme.Colored)
                 .WriteTo.File($"logs/log-{DateTime.Now.Ticks}-", rollingInterval: RollingInterval.Hour)
                 .MinimumLevel.Information()
                 .Enrich.FromLogContext()
