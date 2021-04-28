dfe625d848731f55f7c19e49097f1e31181389d7
Flexlug <flexlug@outlook.com>
2021-04-28 23:26:06 +0300

    Added GUserStatsResponse in Osu-NetApi

---
+++ WAV-Osu-NetApi/Gatari/Models/Responses/GUserStatsResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WAV_Osu_NetApi.Gatari.Models.Responses
+{
+    public class GUserStatsResponse
+    {
+        public int code { get; set; }
+        public GStatistics stats { get; set; }
+    }
+}
