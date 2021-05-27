0b617da3476511cbf89b420eeb0b0048e6335bb1
Flexlug <flexlug@gmail.com>
2021-05-27 05:03:09 +0300

    Added ISheetGenerator and SheetGenerator

---
+++ WAV-Bot-DSharp/Services/Interfaces/ISheetGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAV_Bot_DSharp.Database.Models;
+
+namespace WAV_Bot_DSharp.Services.Interfaces
+{
+    public interface ISheetGenerator
+    {
+        /// <summary>
+        /// Преобразовать все скоры в excel-файл
+        /// </summary>
+        /// <param name="scores">Список всех скоров</param>
+        /// <returns></returns>
+        public Task<FileInfo> CompitScoresToFile(List<CompitScore> scores);
+    }
+}
---
+++ WAV-Bot-DSharp/Services/SheetGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAV_Bot_DSharp.Database.Models;
+using WAV_Bot_DSharp.Services.Interfaces;
+
+namespace WAV_Bot_DSharp.Services
+{
+    public class SheetGenerator : ISheetGenerator
+    {
+        public Task<FileInfo> CompitScoresToFile(List<CompitScore> scores)
+        {
+
+        }
+
+
+    }
+}
