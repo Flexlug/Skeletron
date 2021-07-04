ab4630a3eda1369f7d898eaa5d7fd96f35c0aa51
Flexlug <flexlug@gmail.com>
2021-07-05 00:36:55 +0300

    Allow NF in www

--- WAV-Bot-DSharp/Commands/CompititionCommands.cs
+++ WAV-Bot-DSharp/Commands/CompititionCommands.cs
@@ -422,10 +422,10 @@ namespace WAV_Bot_DSharp.Commands
                 return;
             }
 
-            const int allowedMods = (int)(OsuParsers.Enums.Mods.Perfect | OsuParsers.Enums.Mods.SuddenDeath | OsuParsers.Enums.Mods.ScoreV2);
+            const int allowedMods = (int)(OsuParsers.Enums.Mods.Perfect | OsuParsers.Enums.Mods.SuddenDeath | OsuParsers.Enums.Mods.ScoreV2 | OsuParsers.Enums.Mods.NoFail);
             if (((int)replay.Mods | allowedMods) != allowedMods)
             {
-                await commandContext.RespondAsync("Мы не можем принять данный скор по причине того, что он поставлен с запрещенными на W.W.W модами. \nРазрешенные на W.W.W моды - `SD`, `PF`\nСкор система: V2");
+                await commandContext.RespondAsync("Мы не можем принять данный скор по причине того, что он поставлен с запрещенными на W.W.W модами. \nРазрешенные на W.W.W моды - `SD`, `PF`, `NF`\nСкор система: V2");
                 return;
             }
             
