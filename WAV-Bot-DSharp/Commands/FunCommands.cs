ebf99f717bc4cb613e7e66bf634ba1d6fdd539da
Flexlug <flexlug@gmail.com>
2021-07-08 20:47:45 +0300

    Renamed command: "google" to "lmgt"

--- WAV-Bot-DSharp/Commands/FunCommands.cs
+++ WAV-Bot-DSharp/Commands/FunCommands.cs
@@ -62,7 +62,7 @@ namespace WAV_Bot_DSharp.Commands
             await commandContext.Message.RespondAsync("https://cdn.discordapp.com/attachments/776568856167972904/836541954779119616/4a5b505b4026b6fe30376b0b79d3e108fa755e07r1-540-540_hq.gif");
         }
 
-        [Command("google"), Description("Let me do that job for you")]
+        [Command("lmgt"), Description("Let me do that job for you")]
         public async Task Lmgtfy(CommandContext commandContext,
             [Description("Search querry"), RemainingText] string querry)
         {
