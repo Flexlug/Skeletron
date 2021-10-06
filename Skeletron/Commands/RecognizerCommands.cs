7d13bf8f065bdb9cf8bdd630c9eea2536b2734d4
Flexlug <flexlug@gmail.com>
2021-10-06 17:16:46 +0300

    Commented w.w.w related commands

--- Skeletron/Commands/RecognizerCommands.cs
+++ Skeletron/Commands/RecognizerCommands.cs
@@ -6,6 +6,9 @@ using DSharpPlus.CommandsNext.Attributes;
 using Skeletron.Services.Interfaces;
 
 using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Text;
+using DSharpPlus.Entities;
 
 namespace Skeletron.Commands
 {
@@ -15,12 +18,12 @@ namespace Skeletron.Commands
     [Hidden, RequireGuild]
     public class RecognizerCommands : SkBaseCommandModule
     {
-        private IRecognizerService osu;
+        private IRecognizerService recognizer;
         private ILogger<RecognizerCommands> logger;
 
         public RecognizerCommands(IRecognizerService osu, ILogger<RecognizerCommands> logger)
         {
-            this.osu = osu;
+            this.recognizer = osu;
             this.logger = logger;
 
             logger.LogInformation("RecognizerCommands loaded");
@@ -30,6 +33,50 @@ namespace Skeletron.Commands
         public async Task DummyCommand(CommandContext commandContext)
         {
             await commandContext.RespondAsync("As dummy as me");
+        }
+
+        [Command("ocr"), Description("Permorm an OCR process and recognize text on image. Languages: ENG + RUS")]
+        public async Task OcrImage(CommandContext commandContext)
+        {
+            if (commandContext.Message.Attachments is null || commandContext.Message.Attachments.Count == 0)
+            {
+                await commandContext.RespondAsync("Не обнаружено вложений");
+                return;
+            }
+
+            var attachment = commandContext.Message.Attachments.First();
+            string fileName = attachment.FileName.ToLower();
+
+            // Ignore videofiles
+            if ((fileName.EndsWith(".mp4") ||
+                 fileName.EndsWith(".avi") ||
+                 fileName.EndsWith(".mkv") ||
+                 fileName.EndsWith(".m4v") ||
+                 fileName.EndsWith(".webm") ||
+                 fileName.EndsWith(".mov") ||
+                 fileName.EndsWith(".mts") ||
+                 fileName.EndsWith(".flv") ||
+                 fileName.EndsWith(".3gp") ||
+                 fileName.EndsWith(".m2ts") ||
+                 fileName.EndsWith(".mpg") ||
+                 fileName.EndsWith(".tga")))
+            {
+                await commandContext.RespondAsync("Данное расширение не поддерживается");
+                return;
+            }
+
+            string res = await recognizer.PerformOcr(attachment);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("```");
+            sb.AppendLine(res);
+            sb.AppendLine("```");
+
+            await commandContext.RespondAsync(new DiscordEmbedBuilder()
+                .WithTitle("Распознанный текст:")
+                .WithDescription(sb.ToString())
+                .WithThumbnail(attachment.Url));
         }
     }
 }
