266c32d1e21ba3081412ae31d09868dc24cd4aff
Flexlug <flexlug@gmail.com>
2021-10-07 23:54:01 +0300

    Added VkRegex

---
+++ Skeletron/Converters/VkRegex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WAV_Bot_DSharp.Converters
+{
+    internal class VkRegex
+    {
+        private Regex groupLink { get; set; }
+
+        public VkRegex()
+        {
+            groupLink = new Regex(@"https:\/\/vk.com\/wall-(\d+)_(\d+)");
+        }
+
+        public string TryGetGroupPostId(string msg)
+        {
+            Match match = groupLink.Match(msg);
+
+            if (match is null || match.Groups.Count != 2)
+                return null;
+
+            return $"-{match.Groups[0].Value}_{match.Groups[1].Value}";
+        }
+    }
+}
